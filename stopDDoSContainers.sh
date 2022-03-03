6c3c990ecfa1c0ff79f2202c61332d4bdab2162b
GT <FTowerT@outlook.com>
2022-03-03 16:59:34 +0200

    Bug fixed: We stop containers if they are running

--- stopDDoSContainers.sh
+++ stopDDoSContainers.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-CONTAINERS_IDS=$(docker ps --filter name=DDoS_Attacker -q)
+CONTAINERS_IDS_STRING=$(docker ps --filter name=DDoS_Attacker -q)
 
-echo IDS to stop: $CONTAINERS_IDS
-echo Length: ${#CONTAINERS_IDS[@]}
+echo IDS to stop: $CONTAINERS_IDS_STRING
+echo Length: ${#CONTAINERS_IDS_STRING}
 
 # Check if array length is greater than 0
-if [ ${#CONTAINERS_IDS[@]} -gt 1 ]
+if [ ${#CONTAINERS_IDS_STRING} -gt 1 ]
 then
-
-  docker container stop $CONTAINERS_IDS
-  docker rm $CONTAINERS_IDS
+  docker container stop $CONTAINERS_IDS_STRING
+  docker rm $CONTAINERS_IDS_STRING
 fi
 
 # Print current directory
