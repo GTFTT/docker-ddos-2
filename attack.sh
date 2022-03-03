a94cae6333f80f1aeaa3fcf349ef258b1c932534
GT <FTowerT@outlook.com>
2022-03-03 15:41:23 +0200

    Improved output

--- attack.sh
+++ attack.sh
@@ -10,6 +10,7 @@ randomNumber=$(( $RANDOM % $MAX_NUM + $MIN_NUM ))
 #randomNumber=$(( $(od -An -N4 -i < /dev/urandom) % $MAX_NUM + $MIN_NUM ))
 
 # Script number:
+echo Current datetime: $(date +'%m/%d/%Y') $(date +'%r')
 echo Launching script: $randomNumber
 
 #Stop old DDoS containers
@@ -20,3 +21,4 @@ bash ./stopDDoSContainers.sh
 echo Starting new containers...
 docker-compose --file ./lists/list_$randomNumber/docker-compose.yml up -d
 
+echo ------------------
